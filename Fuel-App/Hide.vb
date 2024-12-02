5fa0a0c6641eec114d540fbcbe24a18c1ab8f9c1
Chris Hadden <chrish2255@gmail.com>
2024-12-02 14:29:23 +0000

    Arrays yayy

--- Fuel-App/Hide.vb
+++ Fuel-App/Hide.vb
@@ -1,7 +1,15 @@
 ﻿Imports System.ComponentModel
 
 Public Class Hidee
+
     Public Shared Sub cleaning(ByVal container As Control)
+        ' Declare a single-dimension array and set its 4 values.
+        Dim numbers = New Integer() {1, 2, 4, 8}
+
+        ' Declare a 4 x 3 multidimensional array and set array element values.
+        Dim matrix = New Integer(,) {{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {4, 5, 6}}
+
+        Dim sales()() As Double = New Double(11)() {}
         For Each txt As TextBox In container.Controls.OfType(Of TextBox)()
             txt.Visible = False
         Next
